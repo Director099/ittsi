72479e606f9ca952f589fadece98a3b68bb56801
redx099 <ferz099@mail.ru>
2024-07-25 12:23:25 +0500

    Завершает типовые страницы

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,8 +1,9 @@
 'use strict';
 
+Fancybox.bind("[data-fancybox]");
+
 import '../blocks/shared/ui/dropdown/dropdown.js';
 import '../blocks/shared/ui/form/form.js';
 import '../blocks/shared/ui/accordion/accordion.js';
-
 import '../blocks/entities/preview/preview.js';
 import '../blocks/entities/search-form/search-form.js';

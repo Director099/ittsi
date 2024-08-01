101abd50be7cabd4660bf65e4703e94a88850d60
redx099 <ferz099@mail.ru>
2024-08-01 12:20:56 +0500

    Вход/Регистрация/Забыли Пароль/Корзина/ 1клик

--- src/js/custom.js
+++ src/js/custom.js
@@ -9,3 +9,11 @@ import '../blocks/shared/ui/tab-filter/tab-filter.js';
 import '../blocks/entities/preview/preview.js';
 import '../blocks/entities/search-form/search-form.js';
 import '../blocks/entities/product-gallery/product-gallery.js';
+
+document.querySelectorAll('[data-btn-eye]').forEach(item =>
+  item?.addEventListener('click', () => {
+    const field = item.parentElement.querySelector('input');
+    item.classList.toggle('field-text__btn-password--open');
+    item.classList.contains('field-text__btn-password--open') ? field.type = 'text' : field.type = 'password';
+  })
+)

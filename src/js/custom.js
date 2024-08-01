b0d327a5df8d8a732d37368559ae0002727073ae
redx099 <ferz099@mail.ru>
2024-08-01 12:22:07 +0500

    Рефакторинг

--- src/js/custom.js
+++ src/js/custom.js
@@ -3,17 +3,10 @@
 import '../blocks/shared/utils-js/calls.js';
 
 import '../blocks/shared/ui/dropdown/dropdown.js';
+import '../blocks/shared/ui/field-text/field-text.js';
 import '../blocks/shared/ui/form/form.js';
 import '../blocks/shared/ui/accordion/accordion.js';
 import '../blocks/shared/ui/tab-filter/tab-filter.js';
 import '../blocks/entities/preview/preview.js';
 import '../blocks/entities/search-form/search-form.js';
 import '../blocks/entities/product-gallery/product-gallery.js';
-
-document.querySelectorAll('[data-btn-eye]').forEach(item =>
-  item?.addEventListener('click', () => {
-    const field = item.parentElement.querySelector('input');
-    item.classList.toggle('field-text__btn-password--open');
-    item.classList.contains('field-text__btn-password--open') ? field.type = 'text' : field.type = 'password';
-  })
-)
